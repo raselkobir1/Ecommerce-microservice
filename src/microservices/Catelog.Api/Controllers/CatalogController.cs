9bcc78ad519c9419427cf6271f7059d4c5db0887
Rasel Kabir <md.rasel@technonext.com>
2024-05-11 22:51:09 +0600

    ++ working on catelog controller.

---
+++ src/microservices/Catelog.Api/Controllers/CatalogController.cs
@@ -0,0 +1,56 @@
+﻿using Catelog.Api.Models;
+using Catelog.Api.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catelog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController(ICatalogRepository catalogRepository) : ControllerBase
+    {
+        // GET: api/<CatalogController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var catalogItems = catalogRepository.GetCatalogItems();
+            return Ok(catalogItems);
+        }
+
+        // GET api/<CatalogController>/653e4410614d711b7fc953a7
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            var catalogItem = catalogRepository.GetCatalogItem(id);
+            return Ok(catalogItem);
+        }
+
+        // POST api/<CatalogController>
+        [HttpPost]
+        public IActionResult Post([FromBody] CatalogItem catalogItem)
+        {
+            catalogRepository.InsertCatalogItem(catalogItem);
+            return CreatedAtAction(nameof(Get), new { id = catalogItem.Id }, catalogItem);
+        }
+
+        // PUT api/<CatalogController>
+        [HttpPut]
+        public IActionResult Put([FromBody] CatalogItem? catalogItem)
+        {
+            if (catalogItem != null)
+            {
+                catalogRepository.UpdateCatalogItem(catalogItem);
+                return Ok();
+            }
+            return new NoContentResult();
+        }
+
+        // DELETE api/<CatalogController>/653e4410614d711b7fc953a7
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            catalogRepository.DeleteCatalogItem(id);
+            return Ok();
+        }
+    }
+}
