66fc44d2a63a30061f44db49126e2980c1d9b018
Rasel Kabir <md.rasel@technonext.com>
2024-05-11 21:24:34 +0600

    ++ add docker compose for mongoDb

---
+++ src/microservices/Catelog.Api/Repository/CatalogRepository.cs
@@ -0,0 +1,27 @@
+﻿using Catelog.Api.Models;
+using MongoDB.Driver;
+
+namespace Catelog.Api.Repository
+{
+    public class CatalogRepository(IMongoDatabase db) : ICatalogRepository
+    {
+        private readonly IMongoCollection<CatalogItem> _col = db.GetCollection<CatalogItem>(CatalogItem.DocumentName);
+        public IList<CatalogItem> GetCatalogItems() =>
+        _col.Find(FilterDefinition<CatalogItem>.Empty).ToList();
+
+        public CatalogItem GetCatalogItem(string catalogItemId) =>
+            _col.Find(c => c.Id == catalogItemId).FirstOrDefault();
+
+        public void InsertCatalogItem(CatalogItem catalogItem) =>
+            _col.InsertOne(catalogItem);
+
+        public void UpdateCatalogItem(CatalogItem catalogItem) =>
+            _col.UpdateOne(c => c.Id == catalogItem.Id, Builders<CatalogItem>.Update
+                .Set(c => c.Name, catalogItem.Name)
+                .Set(c => c.Description, catalogItem.Description)
+                .Set(c => c.Price, catalogItem.Price));
+
+        public void DeleteCatalogItem(string catalogItemId) =>
+            _col.DeleteOne(c => c.Id == catalogItemId);
+    }
+}
---
+++ src/microservices/Catelog.Api/Repository/ICatalogRepository.cs
@@ -0,0 +1,13 @@
+﻿using Catelog.Api.Models;
+
+namespace Catelog.Api.Repository
+{
+    public interface ICatalogRepository
+    {
+        IList<CatalogItem> GetCatalogItems();
+        CatalogItem? GetCatalogItem(string catalogItemId);
+        void InsertCatalogItem(CatalogItem catalogItem);
+        void UpdateCatalogItem(CatalogItem catalogItem);
+        void DeleteCatalogItem(string catagItemId);
+    }
+}
