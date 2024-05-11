d9a97d408877b6e443b53ce19a0705ab49a13e85
Rasel Kabir <md.rasel@technonext.com>
2024-05-11 14:55:24 +0600

    ++Added catelog model and mogoDb package install

---
+++ src/microservices/Catelog.Api/Models/CatalogItem.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catelog.Api.Models 
+{
+    public class CatalogItem
+    {
+        public static readonly string DocumentName = nameof(CatalogItem);
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; init; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
